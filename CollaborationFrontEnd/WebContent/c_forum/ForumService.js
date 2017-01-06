2a2ef4acf31f847403cad3404bc84b4d3bcac0e7
Anki25 <mishrankita25@yahoo.com>
2017-01-06 19:31:55 +0530

    new commit

---
+++ CollaborationFrontEnd/WebContent/c_forum/ForumService.js
@@ -0,0 +1,142 @@
+/**
+ * 
+ */
+'use strict';
+
+app.factory('ForumService',['$http','$q','$rootScope',function($http,$q,$rootScope){
+	
+	console.log("ForumService..")
+	
+	var BASE_URL='http://localhost:6789/CollaborationBackEnd'
+		return{
+		
+		/* GET LIST OF ALL FORUMS */
+		
+fetchAllForums:function(){
+	        console.log("calling fetchAllForums")
+			return $http.get(BASE_URL+'/forums')
+			.then(
+					function(response){
+						return response.data;
+					},
+					function(errResponse){
+						console.error('Error while fetching ForumDetails');
+						return $q.reject(errResponse);
+					}				
+	);
+},
+
+/* GET LIST OF ALL FORUM COMMENTS AS PER ID*/
+
+fetchAllForumComments:function(id){
+	return $http.get(BASE_URL+'/forumscommentlistperforum/'+id)
+	.then(
+			function(response){
+				return response.data;
+			},
+			function(errResponse){
+				console.error('Error while fetching ForumComments List');
+				return $q.reject(errResponse);
+			}				
+);
+},
+
+/* GET LIST OF ALL FORUM COMMENTS */
+
+fetchAllForumComment:function(){
+	return $http.get(BASE_URL+'/forumscommentlist/')
+	.then(
+			function(response){
+				return response.data;
+			},
+			function(errResponse){
+				console.error('Error while fetching ForumComments List');
+				return $q.reject(errResponse);
+			}				
+);
+},
+
+/* CREATE A FORUM COMMENT*/
+
+createComment:function(id,forumcomment){
+	return $http.post(BASE_URL+'/forumcomment/'+id,forumcomment)
+	.then(
+			function(response){
+				return response.data;
+			},
+			function(errResponse){
+				console.error('Error while creating Forum Comment');
+				return $q.reject(errResponse);
+			}				
+);
+},
+
+
+
+/* CREATE A FORUM */
+
+createForum:function(forum){
+	return $http.post(BASE_URL+'/forum/',forum)
+	.then(
+			function(response){
+				return response.data;
+			},
+			function(errResponse){
+				console.error('Error while creating Forum');
+				return $q.reject(errResponse);
+			}				
+);
+},
+
+/* UPDATE A FORUM */
+
+updateForum:function(forum,id){
+	return $http.put(BASE_URL+'/forum/'+id,forum)
+	.then(
+			function(response){
+				return response.data;
+			},
+			function(errResponse){
+				console.error('Error while updating Forum');
+				return $q.reject(errResponse);
+			}				
+);
+},
+
+/* DELETE A FORUM */
+
+deleteForum:function(id){
+	return $http.delete(BASE_URL+'/forum/'+id)
+	.then(
+			function(response){
+				return response.data;
+			},
+			function(errResponse){
+				console.error('Error while deleting Forum');
+				return $q.reject(errResponse);
+			}				
+);
+},
+
+/* GET SELECTED FORUM DETAILS */
+
+getForum: function(id){
+	return $http.get(BASE_URL+'/forum/'+id)
+	.then(                                        //success handler,failure handler
+			function(response){
+				$rootScope.selectedForum=response.data
+				return response.data;
+			},
+			function(errResponse){
+				console.error('Error while getting Forum');
+				return $q.reject(errResponse);
+			}				
+);
+},
+
+
+};  //return
+	
+}
+]
+);
